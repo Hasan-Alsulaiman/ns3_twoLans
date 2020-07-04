a78ebe0e9b173c5a48d4a316134acbafb281b4a3
Hasan-Alsulaiman <careerforma@gmail.com>
2020-07-04 15:20:13 +0300

    added nodes

--- bus.cc
+++ bus.cc
@@ -1,4 +1,4 @@
-// create two separate lans, connect the two with P2P connection, then have one device on lan1 ping a device on lan2
+// create two separate LANs, connect the two with P2P connection, then have one device on LAN1 ping a device on LAN2
 // n1----n2----n3========n1-----n2-----n3
 // |                                   |
 // echo                              echo
@@ -13,27 +13,44 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+NS_LOG_COMPONENT_DEFINE ("P2P");
 
 int
 main (int argc, char *argv[])
 {
   CommandLine cmd;
   cmd.Parse (argc, argv);
-  
+
   Time::SetResolution (Time::NS);
   LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
+  // creating first LAN nodes
   NodeContainer nodes;
   nodes.Create (3);
 
+  // creating second LAN nodes
+  NodeContainer nodes2;
+  nodes2.Create (3);
+
+  // adding nodes from LAN1 and LAN2 to the P2P channel
+  NodeContainer p2pNodes;
+  p2pNodes.Add (nodes.Get (2));
+  p2pNodes.Add (nodes1.Get (0));
+
+  // specs of both LANs
   CsmaHelper csma;
   csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
   csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
 
+  // specs of P2P channel
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
   NetDeviceContainer csmaDevices;
   csmaDevices = csma.Install (nodes);
+  csmaDevices2 = csma.Install (nodes2);
 
   InternetStackHelper stack;
   stack.Install (nodes);
@@ -44,7 +61,6 @@ main (int argc, char *argv[])
   Ipv4InterfaceContainer interfaces = address.Assign (csmaDevices);
 
   UdpEchoServerHelper echoServer (9);
-  
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (0));
   serverApps.Start (Seconds (1.0));
